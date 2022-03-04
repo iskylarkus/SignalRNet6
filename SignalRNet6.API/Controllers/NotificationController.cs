9b9a2262213df22588e04ba2966ce29436f9100b
iskylarkus <turgay.kibar@gmail.com>
2022-03-04 14:07:00 +0300

    myhub handling with ihubcontex in a controller implemented

---
+++ SignalRNet6.API/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRNet6.API.Hubs;
+
+namespace SignalRNet6.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public NotificationController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("{teamCount}")]
+        public async Task<IActionResult> SetTeamCount(int teamCount)
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"Hi Guys, our team must have {teamCount} members only..!");
+
+            return Ok();
+        }
+    }
+}
