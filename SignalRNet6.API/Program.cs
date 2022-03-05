6fda3621a17e819f0e9977835606c0048dd8daca
iskylarkus <turgay.kibar@gmail.com>
2022-03-05 20:50:48 +0300

    sql server connection string adjusted and passed to di container

--- SignalRNet6.API/Program.cs
+++ SignalRNet6.API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using SignalRNet6.API.Hubs;
+using SignalRNet6.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["SqlConStr"]);
+});
 
 builder.Services.AddControllers();
 
