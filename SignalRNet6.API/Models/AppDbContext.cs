e5aefec6f66b1fdb4f97b499cc38539cb47d9690
iskylarkus <turgay.kibar@gmail.com>
2022-03-05 20:50:13 +0300

    dbcontext class created

---
+++ SignalRNet6.API/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRNet6.API.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
