c1320a0219131761eaec5be05fce3a6e0972e153
iskylarkus <turgay.kibar@gmail.com>
2022-03-05 20:49:52 +0300

    user entity created

---
+++ SignalRNet6.API/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRNet6.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual Team Team { get; set; }
+    }
+}
