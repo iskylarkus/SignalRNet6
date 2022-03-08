f772ef2e946cdf4c40a2fe67f70dbb74eb27b27b
iskylarkus <turgay.kibar@gmail.com>
2022-03-08 18:27:15 +0300

    appdbcontex object created and implemented

---
+++ SignalRNet6.CovidChart.API/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRNet6.CovidChart.API.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Covid> Covids { get; set; }
+    }
+}
--- SignalRNet6.CovidChart.API/Program.cs
+++ SignalRNet6.CovidChart.API/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SignalRNet6.CovidChart.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["SqlConStr"]);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
