b1420f58179f9a0bf747ad1893683f9d63297b1f
iskylarkus <turgay.kibar@gmail.com>
2022-03-08 18:05:16 +0300

    myhub class on mvc web developed on client side

--- SignalRNet6.Web/Controllers/HomeController.cs
+++ SignalRNet6.Web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace SignalRNet6.Web.Controllers
             return View();
         }
 
+        public IActionResult IndexWebHub()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
