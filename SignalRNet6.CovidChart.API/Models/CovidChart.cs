eea2afe74a134d42046a7d8f86f6868b8605cdeb
iskylarkus <turgay.kibar@gmail.com>
2022-03-09 17:48:32 +0300

    covid chart class with generating sql query created

---
+++ SignalRNet6.CovidChart.API/Models/CovidChart.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRNet6.CovidChart.API.Models
+{
+    public class CovidChart
+    {
+        public CovidChart()
+        {
+            Counts = new List<int>();
+        }
+
+        public string CovidDate { get; set; }
+        public List<int> Counts { get; set; }
+    }
+}
