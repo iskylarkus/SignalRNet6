87388ebc168e443757a62693c7df18ba14c7b511
iskylarkus <turgay.kibar@gmail.com>
2022-03-09 16:15:20 +0300

    covid service for business layer developed and implemented to di container at startup

---
+++ SignalRNet6.CovidChart.API/Models/CovidService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRNet6.CovidChart.API.Hubs;
+
+namespace SignalRNet6.CovidChart.API.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+            await _context.Covids.AddAsync(covid);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList", "get all from service while adding new values");
+        }
+    }
+}
--- SignalRNet6.CovidChart.API/Program.cs
+++ SignalRNet6.CovidChart.API/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<CovidService>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration["SqlConStr"]);
